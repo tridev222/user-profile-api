ff2c6d9744a8bcddf8725a211a34c5d722cfc318
Devesh Gangwar <gangward82@gmail.com>
2025-03-30 00:43:38 +0530

    user schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 ﻿const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-// Define User Schema
+
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { 
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     profilePic: { type: String, default: "" },
 }, { timestamps: true });
 
-// Hash password before saving
+// password hashing 
 UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
